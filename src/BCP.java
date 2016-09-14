03ead418c548f0ee8728f3cb0e96cf06fb50c0d6
Georgios <Georgios@pc-georgios>
2016-09-14 18:25:13 -0300

    Criação de algumas classes

---
+++ src/BCP.java
@@ -0,0 +1,46 @@
+public class BCP {
+	
+	private String nome;
+	private int X;
+	private int Y;
+	private int PC;
+	private String estado;
+	private int memoria;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getX() {
+		return X;
+	}
+	public void setX(int x) {
+		X = x;
+	}
+	public int getY() {
+		return Y;
+	}
+	public void setY(int y) {
+		Y = y;
+	}
+	public int getPC() {
+		return PC;
+	}
+	public void setPC(int pC) {
+		PC = pC;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	public int getMemoria() {
+		return memoria;
+	}
+	public void setMemoria(int memoria) {
+		this.memoria = memoria;
+	}
+}
---
+++ src/Maquina.java
@@ -0,0 +1,22 @@
+public class Maquina {
+	private int X;
+	private int Y;
+	
+	public int getX() {
+		return X;
+	}
+	
+	public void atribuicaoX(int x) {
+		X = x;
+	}
+	
+	public int getY() {
+		return Y;
+	}
+	
+	public void atribuicaoY(int y) {
+		Y = y;
+	}
+	
+	
+}
