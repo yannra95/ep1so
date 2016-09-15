8f4c76a9fd39d358341243d4e44c032c4154062d
Georgios <Georgios@192.168.0.117>
2016-09-14 22:03:04 -0300

    Mudando ler arquivos.

--- src/Escalonador.java
+++ src/Escalonador.java
@@ -5,36 +5,46 @@ import java.io.IOException;
 
 public class Escalonador {
 	
-	private BCP[] tabelaDeProcessos;
+	private static BCP[] tabelaDeProcessos;
 	
-	public void leArquivo(){
+	public static void leArquivo(){
 		try {
 			
 			File arq = new File("./processos");
-			FileReader arquivo;
-			BufferedReader lerArq;
+			
+			
 			String[] leituraProcessos = arq.list();
 			tabelaDeProcessos = new BCP[leituraProcessos.length];
 			String [] comandos = new String[21];
 			
 			for(int i = 0; i < leituraProcessos.length; i++){
+				System.out.println(leituraProcessos[i]);
+				FileReader arquivo = new FileReader(leituraProcessos[i]);
+				System.out.println(arquivo);
+				BufferedReader lerArq = new BufferedReader(arquivo);
+				System.out.println(lerArq);
+				
+				String linha = lerArq.readLine(); 
+				System.out.println(linha);
+				
+				int cont = 0;
+				while (linha != null) {
+					linha = lerArq.readLine();
+					comandos[cont] = linha;
+					cont++;
+				}
+				
 				BCP bcp = new BCP();
 				tabelaDeProcessos[i] = bcp;
 				tabelaDeProcessos[i].setMemoria(comandos);
-				
-				arquivo = new FileReader(leituraProcessos[i]);
-				lerArq = new BufferedReader(arquivo);
-				String linha = lerArq.readLine(); 
-				
-				while (linha != null) {
-					linha = lerArq.readLine();
-					//comandos
-				}
 			}
+			
 			}catch (IOException e) {
-			System.err.print("Erro na abertura do arquivo: ");
+			System.err.print("Erro na abertura do arquivo. ");
 		}
 	}
 	
-	
+	public static void main (String[] args){
+		leArquivo();
+	}
 }
